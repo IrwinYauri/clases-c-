50089e6c20be4d4cc899f2d97bf8e3fab71563be
Davy Bendezu <djbendezu@gmail.com>
2020-10-05 14:14:15 -0500

    mas C++

---
+++ estrucMemory.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    /*
+    int a=15;
+    bool b; //TRUE, FALSE
+    char c='A'; //ASCII
+    double d=1.2;
+    string cad="irwin"
+    */
+
+    cout<<"int: "<<sizeof(int)<<endl;
+    cout<<"char: "<<sizeof(char)<<endl;
+    cout<<"bool: "<<sizeof(bool)<<endl;
+    cout<<"double: "<<sizeof(bool)<<endl;
+    cout<<"string: "<<sizeof(string)<<endl;
+    
+
+}
